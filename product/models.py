f37de8534a593942743534424693543292cae2f8
SaadEssam <SaadEssam059@gmail.com>
2022-03-10 19:14:49 +0200

    Products App Models

--- product/models.py
+++ product/models.py
@@ -1,4 +1,22 @@
 from django.db import models
-
+from django.utils import timezone
+from django.contrib.auth.models import User
 # Create your models here.
 
+class Product(models.Model):
+  
+  CONDITION_TYPE = (
+    ("New", "New") ,
+    ("Used", "Used")
+  )
+  
+  name = models.CharField(max_length=100)
+  owner = models.ForeignKey(User, on_delete=models.CASCADE)
+  description = models.TextField(max_length=500)
+  condition = models.CharField(max_length=100, choices=CONDITION_TYPE)
+  price = models.DecimalField(max_digits=10, decimal_places=5)
+  created_date = models.DateTimeField(default=timezone.now)
+  
+  def __str__(self):
+    return self.name
+  
