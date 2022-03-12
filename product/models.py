1a6bb2cb1e609209bfef84400c1031d5ff29dc7b
SaadEssam <SaadEssam059@gmail.com>
2022-03-12 19:34:24 +0200

    Setup Static & Media Files

--- product/models.py
+++ product/models.py
@@ -1,3 +1,4 @@
+from contextlib import nullcontext
 from django.db import models
 from django.utils import timezone
 from django.contrib.auth.models import User
@@ -15,6 +16,7 @@ class Product(models.Model):
   description = models.TextField(max_length=500)
   condition = models.CharField(max_length=100, choices=CONDITION_TYPE)
   category = models.ForeignKey('Category', on_delete=models.SET_NULL, null=True)
+  brand = models.ForeignKey('Brand', on_delete=models.SET_NULL, null=True)
   price = models.DecimalField(max_digits=10, decimal_places=5)
   created_date = models.DateTimeField(default=timezone.now)
   
@@ -22,10 +24,23 @@ class Product(models.Model):
     return self.name
   
   
+class ProductImages(models.Model):
+  product = models.ForeignKey(Product, on_delete=models.CASCADE)
+  image = models.ImageField(upload_to='products/', blank=True, null=True)
+  
+  
+  class Meta:
+    verbose_name = 'Product Image'
+    verbose_name_plural = 'Product Images'
+    
+    
+  def __str__(self):
+    return self.product.name
+  
   
 class Category(models.Model):
   category_name = models.CharField(max_length=50)
-  image = models.ImageField(upload_to='products/', blank=True, null=True)
+  image = models.ImageField(upload_to='category/', blank=True, null=True)
   
   
   class Meta:
@@ -34,3 +49,15 @@ class Category(models.Model):
     
   def __str__(self):
     return self.category_name
+  
+  
+
+class Brand(models.Model):
+  brand_name = models.CharField(max_length=50)
+  
+  class Meta:
+    verbose_name = 'brand'
+    verbose_name_plural = 'brands'
+    
+  def __str__(self):
+    return self.brand_name
