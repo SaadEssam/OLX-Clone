04b0d318fed5e926ed78f73fe8b51ce81de6313b
SaadEssam <SaadEssam059@gmail.com>
2022-03-12 18:44:41 +0200

    Add Products Category

--- product/models.py
+++ product/models.py
@@ -14,9 +14,23 @@ class Product(models.Model):
   owner = models.ForeignKey(User, on_delete=models.CASCADE)
   description = models.TextField(max_length=500)
   condition = models.CharField(max_length=100, choices=CONDITION_TYPE)
+  category = models.ForeignKey('Category', on_delete=models.SET_NULL, null=True)
   price = models.DecimalField(max_digits=10, decimal_places=5)
   created_date = models.DateTimeField(default=timezone.now)
   
   def __str__(self):
     return self.name
   
+  
+  
+class Category(models.Model):
+  category_name = models.CharField(max_length=50)
+  image = models.ImageField(upload_to='products/', blank=True, null=True)
+  
+  
+  class Meta:
+    verbose_name = 'category'
+    verbose_name_plural = 'categories'
+    
+  def __str__(self):
+    return self.category_name
