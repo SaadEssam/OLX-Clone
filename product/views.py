dc2f6e28968244664a9895b17c321664ed09ed75
SaadEssam <SaadEssam059@gmail.com>
2022-03-23 15:47:12 +0200

    Adding Products Search

--- product/views.py
+++ product/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from .models import Product, ProductImages, Category
 from django.core.paginator import Paginator
 from django.db.models import Count
+from django.db.models import Q
 
 # Create your views here.
 
@@ -9,9 +10,21 @@ def productlist(request, category_slug=None):
   category = None
   productlist = Product.objects.all()
   categorylist = Category.objects.annotate(total_products=Count('product'))
+  
   if category_slug :
     category = Category.objects.get(slug=category_slug)
     productlist = productlist.filter(category=category)
+    
+  search_query = request.GET.get('q')
+  if search_query :
+    productlist = productlist.filter(
+      Q(name__icontains = search_query)|
+      Q(description__icontains = search_query)|
+      Q(condition__icontains = search_query)|
+      Q(brand__brand_name__icontains = search_query)|
+      Q(category__category_name__icontains = search_query)
+    )
+  
   paginator = Paginator(productlist, 2) 
   page_number = request.GET.get('page')
   productlist = paginator.get_page(page_number) 
