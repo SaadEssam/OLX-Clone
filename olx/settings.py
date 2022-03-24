a2ba05737090880bd8bb8a8035ab3ccfb180280a
SaadEssam <SaadEssam059@gmail.com>
2022-03-24 16:29:49 +0200

    Adding User Accounts

--- olx/settings.py
+++ olx/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     'django.contrib.humanize',
     'ckeditor',
     'product',
+    'accounts',
 ]
 
 MIDDLEWARE = [
@@ -133,3 +134,5 @@ MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+LOGIN_REDIRECT_URL = '/'
